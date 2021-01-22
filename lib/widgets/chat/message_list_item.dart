1edb4cbb45f1b803912c9876b6850007df90411e
Zatt Wine <tomclick.org@gmail.com>
2021-01-23 02:16:23 +0630

    finished peer chat screen

--- lib/widgets/chat/message_list_item.dart
+++ lib/widgets/chat/message_list_item.dart
@@ -2,30 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MessageItem extends StatelessWidget {
-  MessageItem({Key key, this.index, this.message}) : super(key: key);
+import './theme/message_bubble.dart';
+import '../../utils/message_type.dart';
 
-  final int index;
+class MessageItem extends StatelessWidget {
+  MessageItem({Key key, this.message}) : super(key: key);
+
   final QueryDocumentSnapshot message;
-  final String itemTheme = 'default';
   final me = FirebaseAuth.instance.currentUser.uid;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: message['idFrom'] == me
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(message['content']),
-              ],
-            )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(message['content']),
-              ],
-            ),
+      child: MessageBubble(
+        key: ValueKey(message.id),
+        message: message['content'],
+        type: message['type'] == 0
+            ? MessageType.TEXT
+            : message['type'] == 1
+                ? MessageType.IMAGE
+                : MessageType.STICKER,
+        isMe: message['idFrom'] == me,
+        time: message['timestamp'],
+        peerAvatar: message['idFrom'] == me
+            ? message['toAvatar']
+            : message['fromAvatar'],
+      ),
     );
   }
 }
